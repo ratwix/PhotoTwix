b0b450d59608d3054cb6d7c62a02b89574145b0b
Charles <charles.rathouis@gmail.com>
2013-03-17 12:52:28 +0100

    style Photomaton

--- photomaton/src/common.php
+++ photomaton/src/common.php
@@ -5,7 +5,7 @@
 	$root_photo_big = "./result/big";
 	$root_photo_thumb = "./result/thumb";
 	$iview_path = "\"C:\\Program Files (x86)\\IrfanView\\i_view32.exe\"";
-	$full_path_big = "\"C:\\Program Files (x86)\\EasyPHP-12.1\\www\\photomaton\\src\\result\\big";
+	$full_path_big = "\"C:\\Program Files (x86)\\EasyPHP-12.1\\www\\PhotoTwix\\photomaton\\src\\result\\big";
 	$delete_path = "./result/deleted";
 	$real_delete = false;
 ?>
